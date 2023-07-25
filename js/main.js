69dd3b96e093718ce950be806a3ab714ae0ae0b3
angeltrl <angeltrl2000@gmail.com>
2023-07-24 21:51:06 -0300

    al fin addevenlisterner funcionando

--- js/main.js
+++ js/main.js
@@ -4,3 +4,24 @@ let nombre = prompt("Hola!! dime tu nombre");
 
 alert ("Hola " + nombre + " bienvenido :)");
 
+/*const ham = document.querySelector('.ham');
+const enlaces = document.querySelector('.btn-menu');
+
+
+ham.addEventListener('click',function() {
+    enlaces.classList.toggle('activado')
+});*/
+
+window.addEventListener('load', iniciar, false);
+
+function iniciar() {
+    var imagen = document.getElementById('foto'), original = imagen.src;
+
+    imagen.addEventListener('mouseover', function(){
+        this.src = "img/foto2.jpg";  
+    }, false);
+
+    imagen.addEventListener('mouseout', function(){
+        this.src = original;
+    }, false); 
+}
